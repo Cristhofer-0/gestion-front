619e37b728409ddaa35aa2f86c2f7bf02f63ce10
sebaslade <sebaslade@gmail.com>
2025-06-23 14:26:11 -0500

    fix: Arreglo de rutas

---
+++ src/app/dashboard/users/page.tsx
@@ -0,0 +1,45 @@
+"use client"
+import { useState } from "react"
+import { useUser } from "@/hooks/useUser"
+
+//BARRA IZQUIERDA
+import { AppSidebar } from "@/components/AppSideBar/app-sidebar"
+// EL GRAFICO DE LOS DATOS
+import { ChartAreaInteractive } from "@/components/custom/chart-area-interactive"
+//LA TABLA DE LOS DATOS
+import { UserTable } from "@/components/DataTable/data-table-users"
+//LAS CARTAS DE LA PARTE SUPERIOR
+//import { SectionCards } from "./components/section-cards"
+//TOPNAV QUE SE ENCARGA DE MOSTRAR/OCULTAR LA BARRA LATERAL
+import { SiteHeader } from "@/components/custom/site-header"
+//DOCUMENTACION DE SHADCN
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar"
+//LA TABLA DE HISTORIAL
+import { EventHistory } from "@/components/Historial/event-history"
+
+export default function Dashboard() {
+    const user = useUser()
+    const [refreshKey, setRefreshKey] = useState(0)
+
+    const handleHistoryRefresh = () => {
+        setRefreshKey((prev) => prev + 1)
+    }
+
+    if (!user) {
+        return // o puedes mostrar un loader/spinner
+    }
+
+    return (
+        <SidebarProvider>
+            <AppSidebar variant="inset" />
+            <SidebarInset>
+                <SiteHeader />
+                <div className="container mx-auto p-4 md:p-6">
+                    <div className="@container/main flex flex-col gap-6">
+                        <UserTable key={refreshKey} /> 
+                    </div>
+                </div>
+            </SidebarInset>
+        </SidebarProvider>
+    )
+}
