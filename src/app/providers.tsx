34f19a2eb2c10314688c234ea94451251a1a27be
Valentin <mikeallca27@gmail.com>
2025-07-09 12:10:25 -0500

    fix: se arreglo bug de tema dark

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,8 +1,19 @@
 "use client";
 
 import { ThemeProvider } from "next-themes";
-import { ReactNode } from "react";
+import { ReactNode, useEffect, useState } from "react";
 
 export function Providers({ children }: { children: ReactNode }) {
-  return <ThemeProvider attribute="class"  defaultTheme="dark">{children}</ThemeProvider>;
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    // Esto garantiza que el ThemeProvider solo se renderice en el cliente
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return <>{children}</>; // Muestra el contenido normal antes de que se monte el cliente
+  }
+
+  return <ThemeProvider attribute="class" defaultTheme="dark">{children}</ThemeProvider>;
 }
